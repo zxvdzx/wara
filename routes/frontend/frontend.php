e5f98380ffc1213aeef27bfdfe189fe60bc2684e
Ukan Saokani <khnz.97@gmail.com>
2017-11-30 06:07:22 +0700

    add dashboard frontend

--- routes/frontend/frontend.php
+++ routes/frontend/frontend.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'ganteng', 'namespace' => 'Frontend'], function () { 
+    #-- Dashboard
+    Route::get('/', array('as' => 'dashboard', 'uses' => 'DashboardController@index'));
+});
