8b25a402b7f58b6d1e7d5f61509f08d681266def
Ukan Saokani <keeptrying@Ukans-MacBook-Pro.local>
2018-08-21 06:09:27 +0700

    change permission

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 |
 */
 
-require __DIR__.'/../public_html/bootstrap/autoload.php';
+require __DIR__.'/../wara/bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +33,7 @@ require __DIR__.'/../public_html/bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../public_html/bootstrap/app.php';
+$app = require_once __DIR__.'/../wara/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
