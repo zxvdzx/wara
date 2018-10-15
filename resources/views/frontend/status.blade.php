268c28a319653b89e9f365bb1c9ad4ae4ac10813
Ukan Saokani <keeptrying@AAMacBook-Pro.local>
2018-10-15 16:32:59 +0700

    update general data

--- resources/views/frontend/status.blade.php
+++ resources/views/frontend/status.blade.php
@@ -4,25 +4,25 @@
         <div class="stats-info">
             <div class="col-md-3 col-xs-3 stats-grid slideanim">
                 <i class="fa fa-users" aria-hidden="true"></i>
-                <div class='numscroller numscroller-big-bottom' data-slno='1' data-min='0' data-max='12760' data-delay='.5' data-increment="1">12760</div>
+                <div class='numscroller numscroller-big-bottom' data-slno='1' data-min='0' data-max='60' data-delay='.5' data-increment="1">60</div>
                 
                 <h4 class="stats-info">FOREIGN FOLLOWERS</h4>
             </div>
             <div class="col-md-3 col-xs-3 stats-grid slideanim">
                 <i class="fa fa-book" aria-hidden="true"></i>
-                <div class='numscroller numscroller-big-bottom' data-slno='1' data-min='0' data-max='31000' data-delay='.5' data-increment="1">31000</div>
+                <div class='numscroller numscroller-big-bottom' data-slno='1' data-min='0' data-max='5' data-delay='.5' data-increment="1">5</div>
                 
                 <h4 class="stats-info">CLASSES COMPLETE</h4>
             </div>
             <div class="col-md-3 col-xs-3 stats-grid slideanim">
                 <i class="fa fa-trophy" aria-hidden="true"></i>
-                <div class='numscroller numscroller-big-bottom' data-slno='1' data-min='0' data-max='79000' data-delay='.5' data-increment="10">79000</div>
+                <div class='numscroller numscroller-big-bottom' data-slno='1' data-min='0' data-max='1000' data-delay='.5' data-increment="10">1000</div>
                 
                 <h4 class="stats-info">STUDENTS ENROLLED</h4>
             </div>
             <div class="col-md-3 col-xs-3 stats-grid slideanim">
                     <i class="fa fa-user" aria-hidden="true"></i>
-                <div class='numscroller numscroller-big-bottom' data-slno='1' data-min='0' data-max='600' data-delay='.5' data-increment="1">600</div>
+                <div class='numscroller numscroller-big-bottom' data-slno='1' data-min='0' data-max='15' data-delay='.5' data-increment="1">15</div>
             
                 <h4 class="stats-info">CERTIFIED TEACHERS</h4>
             </div>
